66233fdd38e4739561f5267c49cce1a8db63c4f4
zefparis <ben.barere@gmail.com>
2025-10-24 10:10:02 +0200

    update

---
+++ app/src/main/java/com/equisaddles/www/twa/DelegationService.java
@@ -0,0 +1,22 @@
+package com.equisaddles.www.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
+        
+    }
+}
+
